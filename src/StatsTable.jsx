8ac477fa257355db60ada963c59c046861ce6336
Peter Schroedl <peter_schroedl@me.com>
2021-09-12 11:16:20 -0700

    update stats fields

--- src/StatsTable.jsx
+++ src/StatsTable.jsx
@@ -1,14 +1,45 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import PropTypes from 'prop-types';
 
 const dataUrl = 'https://leaderboard-serverless.vercel.app/api/raw_stats?orchestrator=';
 
-const Listitem = ({stat}) => {
+const ListItem = ({ stat }) => {
+  const {
+    region,
+    timestamp,
+    seg_duration,
+    round_trip_time,
+    orchestrator,
+    segments_recieved,
+    segments_sent,
+    transcode_time,
+    upload_time
+  } = stat;
+  const dateTime = new Date(timestamp).toLocaleTimeString('en-US');
+  const isRealTime = seg_duration < round_trip_time ? "yes" : "no"
+
   return (
-    <li key={stat.timestamp}>{stat.region}</li>
+    <li key={timestamp}>
+      <div>region: {region}</div>
+      <div>is realtime?: {isRealTime}</div>
+    </li>
   );
 };
 
+ListItem.propTypes = {
+  stat: {
+    region: PropTypes.string,
+    timestamp: PropTypes.number,
+    seg_duration: PropTypes.number,
+    round_trip_time: PropTypes.number,
+    orchestrator: PropTypes.string,
+    segments_recieved: PropTypes.number,
+    segments_sent: PropTypes.number,
+    transcode_time: PropTypes.number,
+    upload_time: PropTypes.number,
+  },
+};
 class statsTable extends React.Component {
   constructor(props) {
     super(props);
@@ -41,7 +72,9 @@ class statsTable extends React.Component {
         <h5 className="card-header">Test Streams</h5>
         <div className="card-body">
           <ul>
-            {this.state.leaderboardStats.map((stat) => <Listitem key={stat.timestamp} stat={stat}></Listitem>)}
+            {this.state.leaderboardStats.map((stat) => (
+              <ListItem key={stat.timestamp} stat={stat}></ListItem>
+            ))}
           </ul>
         </div>
       </div>
